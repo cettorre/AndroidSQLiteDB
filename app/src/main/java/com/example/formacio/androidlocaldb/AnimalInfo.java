aba0dcc0557992fd4bf3396995d2f8a9b8281e2c
Unknown <c.ettorre@gmail.com>
2018-05-16 13:05:42 +0200

    Working on animal info display
    Created class Animal Info and activity_animal_info corresponding layout

---
+++ app/src/main/java/com/example/formacio/androidlocaldb/AnimalInfo.java
@@ -0,0 +1,13 @@
+package com.example.formacio.androidlocaldb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_info);
+    }
+}
