95ce8812391796c17789fe8227224c10c1b5171f
Unknown <c.ettorre@gmail.com>
2018-05-24 12:53:35 +0200

    writing image to file and converting to BASE64
    added xml file_paths
    added File Provider to manifest
    changed code to save image implementing File Provider
    added convertToBase64 method to convert image to BASE64 string
    changed convertToBase64 to static in order to be visible from onActivityResult

--- app/src/main/java/com/example/formacio/androidlocaldb/InsertAnimal.java
+++ app/src/main/java/com/example/formacio/androidlocaldb/InsertAnimal.java
@@ -9,9 +9,11 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +22,7 @@ import android.widget.SimpleCursorAdapter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -36,7 +39,11 @@ public class InsertAnimal extends AppCompatActivity {
 
     ImageView mImageView;
     Uri photoUri = null;
-    File output;
+
+    Bitmap bm;
+    String encodedImageB64;
+
+
 
     DbHelper mHelper;
     SQLiteDatabase mDb;
@@ -64,7 +71,10 @@ public class InsertAnimal extends AppCompatActivity {
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                capturePhoto("");
+
+                dispatchTakePictureIntent();
+
+
             }
         });
 
@@ -121,66 +131,96 @@ public class InsertAnimal extends AppCompatActivity {
 
 
 
-//TODO  ***************************
-        if (savedInstanceState == null){
-            File dir = getExternalFilesDir(Environment.DIRECTORY_DCIM);
-            dir.mkdirs();
-            output = new File(dir,"com.example.formacio.androidlocaldb.foto1.jpg");
-        } else {
-            output = (File)savedInstanceState.getSerializable("foto1");
-        }
-        //*************************
 
     }
 
-    public void capturePhoto(String targetFilename) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //  intent.putExtra(MediaStore.EXTRA_OUTPUT,                 Uri.withAppendedPath(mLocationForPhotos, targetFilename));
-        //Attempt to invoke virtual method 'android.net.Uri$Builder android.net.Uri.buildUpon()' on a null object reference
 
-//*********TODO
-        photoUri = Uri.fromFile(output);
-        //The name of the Intent-extra used to indicate a content resolver Uri to be used to store the requested image or video.
-     //   intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-    //    startActivityForResult(intent,10);//different req code
-//*********
-        if (intent.resolveActivity(getPackageManager()) != null) {
-                 startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoUri = FileProvider.getUriForFile(this,
+                        "com.example.formacio.androidlocaldb.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+    }
+
+
+    String mCurrentPhotoPath;
+
+    public File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.e("photo_path", mCurrentPhotoPath);
+
+       // String encoded = Base64.encodeFromFile("data/inputImage.png");
+
+   //     encodedImageB64=convertToBase64(mCurrentPhotoPath);
+   //     Log.e("encoded_image",encodedImageB64);
+
+        return image;
+
 
     }
 
-    private String convertToBase64(String imagePath)    {
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            mImageView.setImageURI(photoUri);
+
+            String encodedImage=  InsertAnimal.convertToBase64(mCurrentPhotoPath);
+            Log.e("encoded_image",encodedImage);
+
+       }
+    }
+
+
+
+    public static String convertToBase64(String imagePath)    {
         Bitmap bm = BitmapFactory.decodeFile(imagePath);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         byte[] byteArrayImage = baos.toByteArray();
         String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
         return encodedImage;
+
+
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final Uri mLocationForPhotos= null;//>>asignar para guardar imagen todo
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
-            //capture thumbnail from extra
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-
-            //        Bitmap thumbnail = data.getParcelable("data"); //cannot resolve method
-            // Do other work with full size photo saved in mLocationForPhotos
-
-
-            //get selected image and assign to mImageView after selectImage method
-            Uri fullPhotoUri = data.getData();
-   //         mImageView.setImageURI(fullPhotoUri);
-
-
-        }
-    }
 
 
 
